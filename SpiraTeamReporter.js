132b63d0c12de717c98eca566a30d02b8a6e160c
rick <Rick.Hou@aspiraconnect.com>
2024-03-22 15:25:25 +0800

    0.1.1

--- SpiraTeamReporter.js
+++ SpiraTeamReporter.js
@@ -3,29 +3,29 @@ const { initData } = require("./common");
 const { metric } = require("./metric");
 
 class SpiraTeamReporter {
-    spiraConnectData = {};
-    testCases = {};
-    testRuns = [];
+  spiraConnectData = {};
+  testCases = {};
+  testRuns = [];
 
-    constructor(globalConfig, options) {
-        this._globalConfig = globalConfig;
-        // transfer data from options to testCases and spiraConnectData
-        initData(options, this.testCases, this.spiraConnectData);
-    }
+  constructor(globalConfig, options) {
+    this._globalConfig = globalConfig;
+    // transfer data from options to testCases and spiraConnectData
+    initData(options, this.testCases, this.spiraConnectData);
+  }
 
-    onTestResult(test, testResult, aggregatedResult) {
-        // generate test runs according to testResult and testCases
-        generateTestRuns(testResult, this.testCases, this.testRuns);
-    }
+  onTestResult(test, testResult, aggregatedResult) {
+    // generate test runs according to testResult and testCases
+    generateTestRuns(testResult, this.testCases, this.testRuns);
+  }
 
-    async onRunComplete(contexts, results) {
-        // post test runs to spiraTeam
-        // await postTestRuns(this.spiraConnectData, this.testRuns);
-        // calculate test case count and regression test case count in testFolderId
-        if (this.spiraConnectData.testFolderId) {
-            await metric(this.spiraConnectData, this.testCases);
-        }
+  async onRunComplete(contexts, results) {
+    // post test runs to spiraTeam
+    await postTestRuns(this.spiraConnectData, this.testRuns);
+    // calculate test case count and regression test case count in testFolderId
+    if (this.spiraConnectData.testFolderId) {
+      await metric(this.spiraConnectData, this.testCases);
     }
+  }
 }
 
 module.exports = SpiraTeamReporter;
